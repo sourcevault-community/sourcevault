04f0791d05e98697676ff478a289af3015067080
Justin Ovens <jovens@gotunix.net>
2026-05-19 21:25:36 -0400

    docs(crypto): apply full license header to all files in internal/crypto

--- internal/crypto/signer.go
+++ internal/crypto/signer.go
@@ -9,6 +9,19 @@
 // Mb.     .dM MMb     dMM Mb       dM MM  MMMMM  M MM.     .dM MM        .M M     .dMMM M  MMMMM  MM Mb       dM M         M MMMM  MMMM //
 // MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMM //
 // ===================================================================================================================================== //
+// This program is free software: you can redistribute it and/or modify                                                                  //
+// it under the terms of the GNU Affero General Public License as                                                                        //
+// published by the Free Software Foundation, either version 3 of the                                                                    //
+// License, or (at your option) any later version.                                                                                       //
+//                                                                                                                                       //
+// This program is distributed in the hope that it will be useful,                                                                       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                                         //
+// GNU Affero General Public License for more details.                                                                                   //
+//                                                                                                                                       //
+// You should have received a copy of the GNU Affero General Public License                                                              //
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.                                                                //
+// ===================================================================================================================================== //
 
 // Package crypto provides the in-memory CA signer (the "unseal" mechanism).
 package crypto
